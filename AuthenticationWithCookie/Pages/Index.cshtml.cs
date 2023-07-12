31d6eae7435928e546a9ef3278a327b4356539b9
salma-ahmed9 <93517375+salma-ahmed9@users.noreply.github.com>
2023-07-12 21:48:57 +0200

    code added

---
+++ AuthenticationWithCookie/Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace AuthenticationWithCookie.Pages;
+
+public class IndexModel : PageModel
+{
+    public string ErrorMessage { get; set; }
+    public async Task<IActionResult> OnPost()
+   {
+       string username = Request.Form["myUsername"];
+       string password = Request.Form["myPassword"];
+       if(username==null || password==null) {
+          ModelState.AddModelError("", "username and password fields must be entered");
+          return Page();
+       }
+       if(username== "intern" && password== "summer 2023 july")
+       {
+            var claims = new List<Claim>
+            {
+            new Claim(ClaimTypes.Name, username),
+            new Claim(ClaimTypes.Role, "User"),
+            };
+            var scheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                AllowRefresh = true,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(0.5),
+
+            };
+            var user = new ClaimsPrincipal(claimsIdentity);
+            await HttpContext.SignInAsync(scheme, user, authProperties);
+            return RedirectToPage("/Index");
+        }
+       else
+       {
+            ModelState.AddModelError("", "Invalid username or password");
+            ErrorMessage = "Invalid username or password";
+            return Page();
+        }
+    }
+    public async Task<IActionResult> OnPostLoggingOut()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToPage("/Index");
+    }
+}
